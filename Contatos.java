e98db30f6d02c3c3e2cdfd85ae22870864c2c317
Willian Gabriel <148078844+WilllianGabriel@users.noreply.github.com>
2025-08-01 16:51:31 -0300

    Add files via upload

---
+++ Contatos.java
@@ -0,0 +1,10 @@
+package agendatelefonica;
+
+public class Contatos {
+	
+	String nome;
+	int telefone;
+	
+	//void não pode ser reultilizado.
+	//return é só usado quando o valor precisa ser reutilizado.
+}

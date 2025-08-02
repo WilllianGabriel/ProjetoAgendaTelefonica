c492fcb4f995486d8206813a842db055f04b37ab
Willian Gabriel <148078844+WilllianGabriel@users.noreply.github.com>
2025-08-02 10:42:39 -0300

    updating files
    adicionando funções de guardar e mostrar dados, e menu simples de switch.

--- Contatos.java
+++ Contatos.java
@@ -2,9 +2,31 @@ package agendatelefonica;
 
 public class Contatos {
 	
-	String nome;
-	int telefone;
-	
 	//void não pode ser reultilizado.
 	//return é só usado quando o valor precisa ser reutilizado.
+	
+	private String name;
+	private String telefone;
+	
+	public void mostrar() {
+		System.out.println("Você ainda não tem nenhum Contato!");
+	}
+	
+	//setter e //getter
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	public String getTelefone() {
+		return telefone;
+	}
+	
 }
--- Main.java
+++ Main.java
@@ -1,10 +1,63 @@
 package agendatelefonica;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Ola mundo");
+		
+		// Objeto que permite o usuario escrever na tela.
+		Scanner s = new Scanner(System.in);
+		// Objeto da classe Contatos que permite usar objetos e atributos de la.
+		Contatos c = new Contatos();
+		
+		// \n = quebra de linha
+		System.out.println("Bem vindo a Agenda Telefônica!\n\n"
+				+ "Digite um Número: \n\n"
+				+ "1- Mostrar seus Contatos! \n"
+				+ "2- Adicione, Delete ou Edite um Contato! \n"
+				+ "3- Sair!");
+		
+		int number = s.nextInt();
+		//Usar nextInt ou varianes buga o buffer, fazer um logo após nextLine desbuga!
+		s.nextLine();
+		
+		switch (number) {
+		case 1: {
+			c.mostrar();
+			break;
+		}
+		case 2: {
+			
+			
+			
+			System.out.println("Digite o nome do Contato:");
+			String name1 = s.nextLine();
+			c.setName(name1);
+			
+			System.out.println("Digite o número do Contato:");
+			String telefone1 = s.nextLine();
+			c.setTelefone(telefone1);
+			
+			System.out.println("\n CONTATO SALVO COM SUCESSO!\n\n "
+					+ "Nome do Contato: "+c.getName()
+					+ "\n Número de Telefone: "+c.getTelefone()+"\n\n");
+			
+			break;
+		}
+		case 3: {
+			break;
+		}
+		default:
+			System.out.println("ERRO!, Digito Invalido!, aperte qualquer botão para voltar ao menu!");
+			break;
+		}
+		
+		System.out.println("Fim do Programa!");
 		
 		
+		
+		
+		s.close();
 	}
 }

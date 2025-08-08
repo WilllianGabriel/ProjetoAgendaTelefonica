c4ae84cfdf12913de6fc6227fa64449cf70cca2d
Willian Gabriel <148078844+WilllianGabriel@users.noreply.github.com>
2025-08-08 10:16:44 -0300

    Add files via upload

--- AuthSystem.java
+++ AuthSystem.java
@@ -1,5 +1,20 @@
 package agendatelefonica;
 
 public class AuthSystem {
+	private User registeredUser;
 	
+	public void register(String email, String password) {
+		registeredUser = new User (email,password);
+		System.out.println("Cadastro concluido com sucesso!\n");
+	}
+	public boolean login(String email, String password) {
+		if (registeredUser == null) {
+			System.out.println("Nenhuma Usuário Cadastrado.");
+		}
+		if (registeredUser.getEmail().equals(email) && registeredUser.getPassword().equals(password)) {
+			return true;
+		}else {
+			return false;
+		}
+	}
 }
--- Main.java
+++ Main.java
@@ -13,47 +13,69 @@ public class Main {
 		Contatos c = new Contatos();
 		// Objeto da classe Utils que permite usar objetos e atributos da classe Utils
 		Utils u = new Utils();
-		// Objeto da classe AuthSystem que permite usar objetos e atributos da classe AuthSystem
+		// Objeto da classe AuthSystem que permite usar objetos e atributos da classe
+		// AuthSystem
 		AuthSystem auth = new AuthSystem();
-		// Objeto da classe User que permite usar objetos e atributos da classe User
 		
-		String option = "";
-		while (option != "0") {
-			
+		int optionAuth = 1;
+		int optionMenu = 0;
+		
+		while (optionAuth != 0) {
+
 			System.out.println("\n === Menu ===");
 			System.out.println("1 - Cadastrar");
 			System.out.println("2 - Login");
 			System.out.println("0 - Sair");
 			System.out.println("Escolha uma Opção: ");
-			option = sc.nextLine();
-			
-			switch (option) {
+			String n1 = sc.nextLine();
+			u.verificationNumber(n1);
+
+			switch (n1) {
 			case "1": {
-				
+
 				System.out.println("\n=== Cadastro ===");
 				System.out.println("Digite seu email:");
 				String email = sc.nextLine();
 				System.out.println("Digite seu senha:");
 				String password = sc.nextLine();
-				System.out.println("Cadastro concluido com sucesso!\n");
-				
-				break;
+				auth.register(email, password);
+
 			}
 			case "2": {
-				
+
+				int loopLogin = 0;
+				while (loopLogin != 1) {
+
 					System.out.println("\n=== Login ===");
 					System.out.println("Digite seu email:");
 					String emailLogin = sc.nextLine();
 					System.out.println("Digite seu senha:");
 					String passwordLogin = sc.nextLine();
-					
+					auth.login(emailLogin, passwordLogin);
+					if (auth.login(emailLogin, passwordLogin) == true) {
+						System.out.println("Login feito com sucesso");
+						loopLogin = 1;
+						n1 = "197854263";
+					}else if(auth.login(emailLogin, passwordLogin) == false){
+						System.out.println("Email ou senha invalidos, tente novamente!");
+						System.out.println("\nAperte Enter para Voltar ao menu!");
+						sc.nextLine();
+					}
+				}
 			}
-			case "3": {
-				option = "3";
+			case "0": {
+				optionAuth = 0;
+				//optionMenu = 3;
 				break;
+			} 
+			case "197854263":{
+				
+				
+				
 			}
 			default:
-				//Caso o usuario não digite um número no menu, aqui vai ativar o loop e volta ao menu
+				// Caso o usuario não digite um número no menu, aqui vai ativar o loop e volta
+				// ao menu
 				System.out.println("\nERRO!, Digito Invalido!, Aperte Enter para Voltar ao menu!");
 				sc.nextLine();
 				u.clear();
@@ -61,55 +83,41 @@ public class Main {
 			}
 		}
 		
-		
-		
-		
-		
-		
-		
-		
-
-		//Aqui ja diz o que é!
-	
-		System.out.println("\nComo deseja ser Chamado(a)?");
-		String userName = sc.nextLine();
-		//-------------------------------------------------------------------------
-		
-		// while para fazer loop do menu
-		int option1 = 0;
-		while (option1 != 3) {
-
+		// Aqui ja diz o que é!
+			System.out.println("\nComo deseja ser Chamado(a)?");
+			String userName = sc.nextLine();
+			
+		// while para fazer loop do menu Principal
+		while (optionMenu != 3) {
+			
 			// Aqui é o Menu!
 			System.out.println("\n=== Bem vindo, " + userName + ", A Agenda Telefônica! ===\n\n"
-					+ "Digite um Número: \n\n" 
-					+ "1- Mostrar meus Contatos \n"
-					+ "2- Adicionar um Contato \n"
-					+ "3- Deletar um Contato \n"
-					+ "4- Editar um Contato \n" 
-					+ "5- Sair!");
+					+ "Digite um Número: \n\n" + "1- Mostrar meus Contatos \n" + "2- Adicionar um Contato \n"
+					+ "3- Deletar um Contato \n" + "4- Editar um Contato \n" + "5- Sair");
 
-			String n1 = sc.nextLine();
+			String n2 = sc.nextLine();
 			// objeto que verifica se o que foi digitado foi um número
-			u.verificationNumber(n1);
-			
+			u.verificationNumber(n2);
+
 			// switch para realizar as ações escolhidas pelo usuario
-			switch (n1) {
+			switch (n2) {
 			case "1": {
-				
-				// mostrar = objeto que mostra os contatos salvos, e seus dados como o nome e telefone
+
+				// mostrar = objeto que mostra os contatos salvos, e seus dados como o nome e
+				// telefone
 				c.showContacts();
 				System.out.println("Aperte Enter para Voltar ao menu!");
 				sc.nextLine();
-				//clear = é para pular varias linhas e parecer que limpou a tela
+				// clear = é para pular varias linhas e parecer que limpou a tela
 				u.clear();
 				break;
 			}
 			case "2": {
-				
-				//Cadastro de Contatos!
+
+				// Cadastro de Contatos!
 				System.out.println("Digite o nome do Contato:");
 				String name1 = sc.nextLine();
-				// setName e retorna o valor do Name 
+				// setName e retorna o valor do Name
 				c.setName(name1);
 
 				// while para fazer um loop, caso o que foi digitado não seja número
@@ -119,31 +127,33 @@ public class Main {
 					System.out.println("Digite o número do Contato:");
 					String telefone1 = sc.nextLine();
 					if (u.verificationNumber(telefone1)) {
-						//setTelefone retorna o valor do telefone
+						// setTelefone retorna o valor do telefone
 						c.setTelefone(telefone1);
 						t = 1;
 					} else {
-						System.out.println("\nERRO!, Digito Invalido!, Aperte Enter para Voltar ao " 
+						System.out.println("\nERRO!, Digito Invalido!, Aperte Enter para Voltar ao "
 					+ "menu!");
 						sc.nextLine();
 						u.clear();
 					}
 				}
-				
+
 				// getName e getTelefone chama os valores
-				System.out.println("\nCONTATO SALVO COM SUCESSO!\n\n " + "Nome do Contato: " 
-				+ c.getName() + "\nNúmero de Telefone: " + c.getTelefone() + "\n");
-				System.out.println("Aperte Enter para Voltar ao menu!");
+				System.out.println("\nCONTATO SALVO COM SUCESSO!\n\n " 
+				+ "Nome do Contato: " + c.getName()
+				+ "\nNúmero de Telefone: " + c.getTelefone());
+				System.out.println("\nAperte Enter para Voltar ao menu!");
 				sc.nextLine();
 				u.clear();
 				break;
 			}
 			case "3": {
-				option = "3";
+				optionMenu = 3;
 				break;
 			}
 			default:
-				//Caso o usuario não digite um número no menu, aqui vai ativar o loop e volta ao menu
+				// Caso o usuario não digite um número no menu, aqui vai ativar o loop e volta
+				// ao menu
 				System.out.println("\nERRO!, Digito Invalido!, Aperte Enter para Voltar ao menu!");
 				sc.nextLine();
 				u.clear();
@@ -151,7 +161,7 @@ public class Main {
 			}
 		}
 		
-		//Aqui ja diz o que é!
+		// Aqui ja diz o que é!
 		System.out.println("\nFim do Programa!");
 
 		sc.close();
